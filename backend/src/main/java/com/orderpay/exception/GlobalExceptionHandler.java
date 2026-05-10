a6c18f71edbf1bdc74cee38e6f2a3a23f5f4a9d5
Brajraj devda <147842418+BrajRaj89@users.noreply.github.com>
2026-05-10 13:47:59 +0530

    add mysql database

--- backend/src/main/java/com/orderpay/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/orderpay/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.orderpay.exception
+package com.orderpay.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
